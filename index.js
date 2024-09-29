563377ef36170d9d0f1c91d8e7dddf9fb5e3e362
Swapnanil Manna <127471920+Swapnanilmanna1701@users.noreply.github.com>
2024-09-29 15:58:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed(".home",{
+    strings: ["I,m Supriti Mondal a First Year Undergrad There At Techno Main Salt Lake From The Depertment Of Bachelorin Computer Application (BCA)."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
